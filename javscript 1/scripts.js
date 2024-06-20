00d7e79aac606640911c457a14f19db9c4092492
ezequielpamich <ezequiel.blanco@hotmail.com.ar>
2024-06-20 00:31:52 -0300

    Add files via upload

---
+++ javscript 1/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+// Puedes agregar scripts personalizados aquí
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Ejemplo de script: Mostrar alerta al cargar la página
+    console.log("Página cargada correctamente");
+  });
+  
+  function mostrarMensajeDeBienvenida() {
+    alert("¡Bienvenido a mi página web!");
+}
+
+window.onload = mostrarMensajeDeBienvenida;
